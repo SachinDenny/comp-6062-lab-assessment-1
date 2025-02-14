b3d788ee036a5a8e3bca9610a416c0b01808ed7a
SachinDenny <sachindenny2@gmail.com>
2025-02-13 20:07:11 -0500

    updated

--- scripts/app.js
+++ scripts/app.js
@@ -3,5 +3,5 @@ const studentNumber =1196251
 const result=studentFullName+'-'+studentNumber;
 console.log(result)
 let headerContent =document.querySelector(".message")
-headerContent.innerHTML(result)
+headerContent.innerHTML=result
 headerContent.classList.add("headingPrimary")

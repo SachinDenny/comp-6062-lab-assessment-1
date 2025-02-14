51c3f464eb696ca42f28a8beeb27ec45bfb41834
SachinDenny <sachindenny2@gmail.com>
2025-02-13 19:43:27 -0500

    up

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+const studentFullName ='Sachin Denny';
+const studentNumber =1196251
+const result=studentFullName+'-'+studentNumber;
+console.log(result)
+let headerContent =document.querySelector(".message")
+headerContent.innerHTML(result)
+headerContent.classList.add("headingPrimary")
